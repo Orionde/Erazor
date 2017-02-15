5243919dc930dceb956fdc4f40623ba9c0e2ca00
congiohj <Jorane.Congio-Hollard@etu.upmf-grenoble.fr>
2017-02-15 09:56:29 +0100

    Delete reinit_file_status.sh

--- reinit_file_status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo > info_files/ID_old.txt
-echo > info_files/ID_new.txt
-echo > info_files/fdisk_old.txt
-echo > info_files/fdisk_new.txt
-megasasctl -vvv > info_files/mega_old.txt
-megasasctl -vvv > info_files/mega_new.txt
