aabe660f89a9deb036e97fc5ef6fd8a7b0fa20d2
Jorane Congio <jorane.congio@gmail.com>
2017-03-07 16:30:17 +0100

    test mail

---
+++ test_erazor/test.sh
@@ -0,0 +1,1 @@
+bash test_mail.sh jco@oxalide.com jco@oxalide.com test_pouet_test 10.1.71.103
--- test_erazor/test_mail.sh
+++ test_erazor/test_mail.sh
@@ -1,20 +1,46 @@
-from=$1
-to=$2
-subject=$3
-data=$4
+#from=$1
+#to=$2
+#data=$3
+#domaine=$4
+#host=$(hostname)
 
-#domaine=`echo $to | cut -d '@' -f2`
-
-#mx_princ=`dig MX $domaine | grep -v "^;" | grep MX | awk {'print $5,$6'} | sort | head -n 1 | awk {'print $2'}`
-
-domaine=$5
+from="jorane.congio@oxalide.com"
+to="jorane.congio@oxalide.com"
+data=$(cat disks.txt)
+domaine=10.1.71.103
+host=$(hostname)
 
 (
-	 echo "EHLO"
-	 echo "MAIL FROM:<$from>"
+	 echo "EHLO $host"
+	 sleep 2
+
+	 echo "MAIL FROM:$from"
+	 sleep 2
+	 
 	 echo "RCPT TO:$to"
-	 echo "data"
-	 echo "subject:$subject"
+	 sleep 2
+	 
+	 echo "DATA"
+	 sleep 2
+
+	 echo "Subject: Disks"
+	 sleep 2
+	 
+	 echo
+	 sleep 2
+	 
+	 echo
+	 sleep 2
+
+	 echo ""
+	 
 	 echo "$data"
+	 sleep 2
+	 
+	 echo
+	 sleep 2
+	 
 	 echo "."
+	 sleep 2
+	 
  ) | telnet $domaine 25
